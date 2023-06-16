b9562ac8bdb89b75246c0b03d3730986fae97e02
Deolinda <deolindameyer5@gmail.com>
2023-06-16 07:13:38 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that prints last number of n
+ * Return:0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	char[] charN = n.toString().ToCharArray();
+	int size = charN.Length;
+	int lastNumb = charN[size - 1];
+
+	if (lastNumb < 5)
+	{
+		printf("Last digit of %d is %d and is less than 5 and not 0\n", n, lastNumb);
+	}
+	else if (lastNumb == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastNumb);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastNumb);
+	}
+	return (0);
+}

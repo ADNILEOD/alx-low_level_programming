3f3a28a1d93872313e3c2038487b1dcc3a213b04
Deolinda <deolindameyer5@gmail.com>
2023-06-16 07:33:38 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	char charN[] = n.toString().ToCharArray();
+	char charN[] = n.ToString().ToCharArray();
 	int size = charN.Length;
 	int lastNumb = Integer.parse(charN[size - 1]);
 

5fbf3a5f8e8bb32b6e8ad1df174bfc6d09eaf5d6
Deolinda <deolindameyer5@gmail.com>
2023-06-18 08:35:37 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Description
+ * main - A program that print numbers of base 10
+ * Return:0
+ */
+int main(void)
+{
+	int n = 9;
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n = n + 1;
+	}
+	putchar('\n');
+	return (0);
+}

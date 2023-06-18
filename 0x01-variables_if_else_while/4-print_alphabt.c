b56c1a60b216c3824ceaac1095ddf9aff8ca7c9a
Deolinda <deolindameyer5@gmail.com>
2023-06-18 06:46:06 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a alphabet
+ * Return:0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 65; chr <= 97; chr = chr + 1)
+	{
+		if (chr > 90 && chr < 97)
+		{
+			continue;
+		}
+		if (chr != 101 || chr != 113)
+		{
+		putchar(chr);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

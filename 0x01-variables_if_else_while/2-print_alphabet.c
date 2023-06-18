5283591f044db6432f62ab4649c0d96ff8993cda
Deolinda <deolindameyer5@gmail.com>
2023-06-18 05:35:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a alphabet
+ * Return:0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 65; chr <= 122; chr = chr + 1)
+	{
+		if (chr > 90 && chr < 97)
+		{
+			continue;
+		}
+		putchar("%c\n", chr);
+	}
+	return (0);
+}

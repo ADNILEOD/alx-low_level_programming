9a7c38e34d92a1128d4c834a8c872b8466ccfc67
Deolinda <deolindameyer5@gmail.com>
2023-06-26 05:03:33 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,7 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0
+	int longi = 0;
 		int o;
 
 	while (*s != '\0')
@@ -20,5 +20,5 @@ void print_rev(char *s)
 		_putchar(*s);
 		s--;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
